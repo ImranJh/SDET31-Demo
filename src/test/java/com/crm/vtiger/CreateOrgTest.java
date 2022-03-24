b482b955f83f0182a71a1b90183b6f15cb4d044e
HP <HP@LAPTOP-F1SH0AT3>
2022-03-24 14:42:38 +0530

    modified create org

--- src/test/java/com/crm/vtiger/CreateOrgTest.java
+++ src/test/java/com/crm/vtiger/CreateOrgTest.java
@@ -13,6 +13,16 @@ public class CreateOrgTest {
 	public void organization()
 	{
 		System.out.println("Organization Created By Imran");
+		
+		System.out.println("M");
+		System.out.println("O");
+		System.out.println("D");
+		System.out.println("I");
+		System.out.println("F");
+		System.out.println("I");
+		System.out.println("E");
+		System.out.println("D");
+
 	}
 	
 	
