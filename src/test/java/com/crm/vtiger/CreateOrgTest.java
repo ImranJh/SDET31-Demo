959af89c48069ff59d7f3768b26b8c5333e017cb
HP <HP@LAPTOP-F1SH0AT3>
2022-03-24 12:17:16 +0530

    User1 first commit

---
+++ src/test/java/com/crm/vtiger/CreateOrgTest.java
@@ -0,0 +1,13 @@
+package com.crm.vtiger;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+
+	@Test
+	public void createOrg()
+	{
+		System.out.println("org created");
+	}
+	
+}
