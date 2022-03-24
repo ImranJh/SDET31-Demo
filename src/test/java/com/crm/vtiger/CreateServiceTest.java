7d260ae2c31e53a41489afb575e0f00801f1612b
HP <HP@LAPTOP-F1SH0AT3>
2022-03-24 12:29:58 +0530

    User1 second commit

---
+++ src/test/java/com/crm/vtiger/CreateServiceTest.java
@@ -0,0 +1,13 @@
+package com.crm.vtiger;
+
+import org.testng.annotations.Test;
+
+public class CreateServiceTest {
+	
+	@Test
+	public void createProduct()
+	{
+		System.out.println("product created");
+	}
+
+}
